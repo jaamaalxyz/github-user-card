698f95478ae2fc5444ddbfade5b385dd54c2486f
jamal-pb95 <jamal.pb95@gmail.com>
2018-11-22 02:17:26 +0600

    Updates

---
+++ precache-manifest.8e2e6ebf99592914e6e653d8a0308646.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b043097ba777b588b483cfd3ac3c364",
+    "url": "/github-user-card/static/media/reactgithub.6b043097.jpg"
+  },
+  {
+    "revision": "9c4d07acf727201b06fe",
+    "url": "/github-user-card/static/js/runtime~main.9c4d07ac.js"
+  },
+  {
+    "revision": "4ba4a4a7cdff44789c94",
+    "url": "/github-user-card/static/js/main.4ba4a4a7.chunk.js"
+  },
+  {
+    "revision": "1a41f4ec07e4f556afa1",
+    "url": "/github-user-card/static/js/1.1a41f4ec.chunk.js"
+  },
+  {
+    "revision": "4ba4a4a7cdff44789c94",
+    "url": "/github-user-card/static/css/main.073395e6.chunk.css"
+  },
+  {
+    "revision": "3e9fa80836033810087cc339e75d4753",
+    "url": "/github-user-card/index.html"
+  }
+];
