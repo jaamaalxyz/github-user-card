1fa1eb7ced74ffc37c006fa4375abebfe3822aeb
jamal-pb95 <jamal.pb95@gmail.com>
2018-11-21 19:06:52 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,76 +1,28 @@
-import React from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const Card = props => {
-  return (
-    <div style={{ margin: '1em' }}>
-      <img alt="avatar" style={{ width: '70px' }} src={props.avatar_url} />
-      <div>
-        <div style={{ fontWeight: 'bold' }}>{props.name}</div>
-        <div>{props.blog}</div>
-      </div>
-    </div>
-  )
-}
-
-const CardList = props => {
-  return <div>{props.cards.map(card => <Card {...card} />)}</div>
-}
-
-class Form extends React.Component {
-  state = {
-    userName: ''
-  }
-
-  handleSubmit = event => {
-    event.preventDefault()
-
-    axios
-      .get(`https://api.github.com/users/${this.state.userName}`)
-      .then(resp => {
-        this.props.onSubmit(resp.data)
-        this.setState({ userName: '' })
-      })
-  }
-
+class App extends Component {
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          value={this.state.userName}
-          onChange={event => this.setState({ userName: event.target.value })}
-          placeholder="GitHub username"
-          required
-        />
-        <button type="submit">Add card</button>
-      </form>
-    )
-  }
-}
-
-class App extends React.Component {
-  state = {
-    cards: []
-  }
-
-  addNewCard = cardInfo => {
-    this.setState(prevState => ({
-      cards: prevState.cards.concat(cardInfo)
-    }))
-  }
-
-  render() {
-    return (
-      <div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <Form onSubmit={this.addNewCard} />
-        <CardList cards={this.state.cards} />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
