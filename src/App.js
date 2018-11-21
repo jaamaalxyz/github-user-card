943408e15f780428aba0cfc8bbd115249d9b771b
jamal-pb95 <jamal.pb95@gmail.com>
2018-11-21 18:58:41 +0600

    app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,76 @@
-import React, { Component } from 'react';
+import React from 'react'
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const Card = props => {
+  return (
+    <div style={{ margin: '1em' }}>
+      <img alt="avatar" style={{ width: '70px' }} src={props.avatar_url} />
+      <div>
+        <div style={{ fontWeight: 'bold' }}>{props.name}</div>
+        <div>{props.blog}</div>
+      </div>
+    </div>
+  )
+}
+
+const CardList = props => {
+  return <div>{props.cards.map(card => <Card {...card} />)}</div>
+}
+
+class Form extends React.Component {
+  state = {
+    userName: ''
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    axios
+      .get(`https://api.github.com/users/${this.state.userName}`)
+      .then(resp => {
+        this.props.onSubmit(resp.data)
+        this.setState({ userName: '' })
+      })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.userName}
+          onChange={event => this.setState({ userName: event.target.value })}
+          placeholder="GitHub username"
+          required
+        />
+        <button type="submit">Add card</button>
+      </form>
+    )
   }
 }
 
-export default App;
+class App extends React.Component {
+  state = {
+    cards: []
+  }
+
+  addNewCard = cardInfo => {
+    this.setState(prevState => ({
+      cards: prevState.cards.concat(cardInfo)
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <Form onSubmit={this.addNewCard} />
+        <CardList cards={this.state.cards} />
+      </div>
+    )
+  }
+}
+
+export default App
